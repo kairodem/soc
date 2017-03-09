3d24b0db78c8e4714a1fe2aa1be9b4930f44d2e6
unknown <abdallah_3addoula@hotmail.com>
2017-03-09 11:49:15 +0100

    added index.php and assets folder

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link href="https://fonts.googleapis.com/css?family=Quicksand:300,400,500,700" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="assets/css/main.css">
+	<link rel="stylesheet" type="text/css" href="assets/css/reset.css">
+	<script type="text/javascript" src="assets/scripts/app.js"></script>
+</head>
+<body>
+	<div class="header">
+		<div class="wrapper">
+			<div class="logo"><a href="#"><h1>NetName</h1></a></div>
+			<div class="rightlist">
+				<ul>
+					<li id="l1"><a href="#">Join</a></li>
+					<li id="sep"></li>
+					<li id="l2"><a href="#">Forgot username?</a></li>
+				</ul>
+			</div>
+		</div>
+	</div>
+	<div class="content">
+		<div class="wrapper">
+			<center>
+				<h1>Socialising.. Like never before.</h1>
+			</center>
+				<div class="first">
+					<h2>Things happen here just like in the real world, it's more of a simulation of society, it's all about the community.</h2>
+					<ul>
+						<li><span>*</span><strong>No friend requests</strong>. Once you join the network, that's it, you're a part of the community.</li>
+						<li><span>*</span><strong>No blocking</strong>. But you can always report any viloations.</li>
+						<li><span>*</span><strong>No message blocking</strong>. Someone is annoying you? tell them to stop it.</li>
+						<li><span>*</span><strong>No restrictions</strong> other than Terms Of Use.</li>
+					</ul>
+					<h3>We do not tolerate <strong>scamming, spamming,</strong> ot <strong>any other way</strong> of disturbing the community.</strong><a href=""> See more.</a></h3>
+				</div>
+		</div>
+	</div>
+</body>
+</html>
